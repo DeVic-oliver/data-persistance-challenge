7d5c7644b90175e294e51c054fe9b7a0848ea3c1
Victor Matheus <victormatheus183@gmail.com>
2022-03-20 20:53:42 -0300

    Exit method added
    Just a 'Exit' funcionality made for the player close the game.

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor;
 public class MenuUIHandler : MonoBehaviour
 {
     //This is the handler of the main menu scene
-    
+
     [SerializeField] Text PlayerNameInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     public void StartGame()
     {
@@ -22,4 +23,13 @@ public class MenuUIHandler : MonoBehaviour
     {
         PlayerDataHandle.Instance.PlayerName = PlayerNameInput.text;
     }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    } 
 }
